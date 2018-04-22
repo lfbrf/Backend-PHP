38c40340ffeb9d1d6b6ac0256e69d677d5e920d2
Luiz Felipe <luizfelipebasile@gmail.com>
2018-04-22 18:10:03 -0300

    Add seed users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,250 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+    	DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'city' => str_random(10),
+            'state' => str_random(2),
+            'postal_code' => str_random(10),
+            'address' => str_random(10),
+            'number' => rand(5, 15),
+            'complement' => str_random(10),
+            'district' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+     
     }
 }
+ 
